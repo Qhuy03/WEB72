67bcde36ba2d2d7ca4dd3c252d8c73c6bd9c4f72
Qhuy03 <huyntq2003@gmail.com>
2023-10-03 00:07:00 +0700

    middle

--- README.md
+++ README.md
@@ -1,42 +1,58 @@
-SSR vs CSR
-Router: bộ định tuyến
+-SSR vs CSR
+-Router: bộ định tuyến
 
-import Expess
-Browser(GET)
-CRUD
-HTTP vs HTTPS
+-import Expess
+-Browser(GET)
+-CRUD
+-HTTP vs HTTPS
 
              HTTP
 Client -----------------------> Server 
             call API
 
 
-restful API 
+-restful API 
 
 
-các kiểu export
+-các kiểu export
 
 module.exports = Expess
 module.exports = {a,b,c}
 
-status code
+-status code
 
 
-HTTP Method:{
+-HTTP Method:{
     GET: lấy data
     POST-PATCH: gửi data
     PUT: update
     DELETE: xóa
 }
 
-sql          vs             nosql
-db           |               db
-table        |              collection 
-column       |               field
-row/record   |              document
+    sql           vs             nosql
+    db            |               db
+    table         |              collection 
+    column        |               field
+    row/record    |              document
 
 
-Bất đồng bộ:
+-Bất đồng bộ:
 
 
-Cách xử lý: callback, promise, async/await"# WEB72" 
+-Cách xử lý: callback, promise, async/await
+
+
+-LocalStorage, Session, Cookie
+
+-MVC
+
+-JWT, Token (access token vs refresh token)
+
+-Middleware: function(req, res, next)
+
+-Authorization vs Authentication
+
+
+-Solid (OOP)
+-ACID
+-cors
--- index.js
+++ index.js
@@ -10,28 +10,69 @@ app.use(router);
 
 
 
-const products = [1, 2, 3];
+// const products = [1, 2, 3];
 
-app.get('/product', (req, res) => {
-   return res.status(200).json(products);
+// app.get('/product', (req, res) => {
+//    return res.status(200).json(products);
+// });
+
+// app.post('/product', (req, res) => {
+//    return res.status(201).json({ message: 'Create success' });
+// });
+
+// app.put('/product/:id', (req, res) => {
+//     const productId = req.params.id;
+//     console.log('ID tương ứng là: ', productId);
+//     return res.status(200).json({message: 'Update success'});
+// });
+
+// app.delete('/product/:id', (req, res) => {
+//     const productId = req.params.id;
+//     console.log('ID tương ứng:', productId);
+//     return res.status(200).json({message: 'Remove success'});
+// })
+
+const user = [
+	{username: 'alice', apiKey: 'alice@123'},
+  {username: 'bob', apiKey: 'bob@123'},
+  {username: 'charlie', apiKey: 'charlie@123'}
+];
+
+let statistics = [
+    { user: 'alice', student: 0, teacher: 0, subject: 0 },
+    { user: 'bob', student: 0, teacher: 0, subject: 0 },
+    { user: 'charlie', student: 0, teacher: 0, subject: 0 }
+];
+
+app.use((req, res, next) => {
+    console.log(`[${new Date().toLocaleString()}] ${req.method} ${req.url}`);
+  
+    
+    const apiKey = req.query.apiKey;
+    const user = users.find(u => u.apiKey === apiKey);
+  
+    if (!user) {
+      return res.status(401).json({ message: 'Authentication failed' });
+    }
+  
+    
+    const index = users.findIndex(u => u.username === user.username);
+    if (index !== -1) {
+      if (req.url.includes('/student')) {
+        statistics[index].student++;
+      } else if (req.url.includes('/teacher')) {
+        statistics[index].teacher++;
+      } else if (req.url.includes('/subject')) {
+        statistics[index].subject++;
+      }
+    }
+  
+    next(); 
 });
 
-app.post('/product', (req, res) => {
-   return res.status(201).json({ message: 'Create success' });
+app.get('/system', (req, res) => {
+    res.status(200).json(statistics);
 });
-
-app.put('/product/:id', (req, res) => {
-    const productId = req.params.id;
-    console.log('ID tương ứng là: ', productId);
-    return res.status(200).json({message: 'Update success'});
-});
-
-app.delete('/product/:id', (req, res) => {
-    const productId = req.params.id;
-    console.log('ID tương ứng:', productId);
-    return res.status(200).json({message: 'Remove success'});
-})
-
 
 app.listen(3001, ()=>{
     console.log("server is running!");
