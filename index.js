01e19648f0554ed19ce1741774e1b6ed2164ec36
Qhuy03 <huyntq2003@gmail.com>
2023-10-03 00:13:41 +0700

    middle

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express'); // commonjs
 const app = express();// khởi chạy dự án
 const morgan = require('morgan');
 const router = require('./routers');
+const userMDL = require('./middlewares/userMDL')
 
 
 app.use(morgan("combined")); 
@@ -44,33 +45,11 @@ let statistics = [
     { user: 'charlie', student: 0, teacher: 0, subject: 0 }
 ];
 
-app.use((req, res, next) => {
-    console.log(`[${new Date().toLocaleString()}] ${req.method} ${req.url}`);
-  
+// app.use((req, res, next) => {
     
-    const apiKey = req.query.apiKey;
-    const user = users.find(u => u.apiKey === apiKey);
-  
-    if (!user) {
-      return res.status(401).json({ message: 'Authentication failed' });
-    }
-  
-    
-    const index = users.findIndex(u => u.username === user.username);
-    if (index !== -1) {
-      if (req.url.includes('/student')) {
-        statistics[index].student++;
-      } else if (req.url.includes('/teacher')) {
-        statistics[index].teacher++;
-      } else if (req.url.includes('/subject')) {
-        statistics[index].subject++;
-      }
-    }
-  
-    next(); 
-});
+// });
 
-app.get('/system', (req, res) => {
+app.get('/system/statistic',userMDL, (req, res) => {
     res.status(200).json(statistics);
 });
 
--- middlewares/userMDL.js
+++ middlewares/userMDL.js
@@ -1,2 +1,29 @@
 const fs = require('fs');
 
+const userMDL = function (req, res, next) {
+    console.log(`[${new Date().toLocaleString()}] ${req.method} ${req.url}`);
+  
+    
+    const apiKey = req.query.apiKey;
+    const user = users.find(u => u.apiKey === apiKey);
+  
+    if (!user) {
+      return res.status(401).json({ message: 'Authentication failed' });
+    }
+  
+    
+    const index = users.findIndex(u => u.username === user.username);
+    if (index !== -1) {
+      if (req.url.includes('/student')) {
+        statistics[index].student++;
+      } else if (req.url.includes('/teacher')) {
+        statistics[index].teacher++;
+      } else if (req.url.includes('/subject')) {
+        statistics[index].subject++;
+      }
+    }
+  
+    next(); 
+};
+
+module.exports = userMDL;
